00b81c0469de301f9baf75463f6b68e3b44a59a8
kkapsner <github@mail.kkapsner.de>
2015-03-23 15:36:34 +0100

    Initial Commit

---
+++ addNetworkFavorite/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  addNetworkFavorite
+//
+//  Created by Kapsner, Korbinian on 23.03.15.
+//  Copyright (c) 2015 Kapsner, Korbinian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

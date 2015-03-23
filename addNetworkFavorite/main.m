4a975c1caa12bc7acbf7b216598dcbb8863d3a3a
kkapsner <github@mail.kkapsner.de>
2015-03-23 15:38:05 +0100

    First draft

--- addNetworkFavorite/main.m
+++ addNetworkFavorite/main.m
@@ -10,8 +10,33 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        if (argc < 2){
+            NSLog(@"One arguments required: mounted volume path. Second argument (display name) optional.");
+            return 1;
+        }
+        else {
+            NSString *mountPath = [NSString stringWithCString:argv[1] encoding:[NSString defaultCStringEncoding]];
+            NSString *displayName;
+            if (argc < 3){
+                displayName = NULL;
+            }
+            else {
+                displayName = [NSString stringWithCString:argv[2] encoding:[NSString defaultCStringEncoding]];
+            }
+            NSLog(@"Adding favorite: %@ (%@)", mountPath, displayName);
+            
+            LSSharedFileListRef favoriteItems = LSSharedFileListCreate(NULL, kLSSharedFileListFavoriteItems, NULL);
+            
+            
+            CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:mountPath];
+            
+            
+            LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(favoriteItems, kLSSharedFileListItemLast, (__bridge CFStringRef)(displayName), NULL, url, NULL, NULL);
+            
+            if (item){
+                CFRelease(item);
+            }
+        }
     }
     return 0;
 }
